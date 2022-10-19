4757bd0b4feb8be896c94b94cdb34ec01522013f
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2022-10-19 21:34:06 +0400

    Update Point3D.java

--- Chapter 2/Pract 3.2/PointXD (3.2.5)/Point3D.java
+++ Chapter 2/Pract 3.2/PointXD (3.2.5)/Point3D.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package extention;
 
-/**
- *
- * @author Ilia_Dukhov
- */
 public class Point3D {
-    
+    private int z;
+    public Point3D(int x, int y, int z){
+        super(x,y);
+        this.z = z;
+    }
 }
