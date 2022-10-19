73b3bddf9dc6531dbd54b99b1dc3756771507dab
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2022-10-19 21:34:59 +0400

    Update Extention.java

--- Chapter 2/Pract 3.2/PointXD (3.2.5)/Extention.java
+++ Chapter 2/Pract 3.2/PointXD (3.2.5)/Extention.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package extention;
 
-/**
- *
- * @author Ilia_Dukhov
- */
 public class Extention {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        PointXD p1 = new PointXD();
+        p1.props.add(new Property("x", 3));
+        
     }
     
 }
