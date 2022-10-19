60b317ba03cee01557618503a7a2721c8db5aba9
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2022-10-19 21:37:57 +0400

    Update Point.java

--- Chapter 2/Pract 3.2/PointXD (3.2.5)/Point.java
+++ Chapter 2/Pract 3.2/PointXD (3.2.5)/Point.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package extention;
-
-/**
- *
- * @author Ilia_Dukhov
- */
 public class Point {
-    
+     private int x,y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public String toString(){
+        return String.format("{%d : %d}", x,y);
+    }
 }
