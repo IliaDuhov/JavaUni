d628b98f515fdaa2ea03ea71c8e344effffee996
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2022-11-24 19:34:36 +0400

    Add files via upload

---
+++ DifferentTasks/Convertor/Convertor.java
@@ -0,0 +1,14 @@
+
+package repeaettest;
+
+
+abstract class Convertor{
+    abstract void read();
+    abstract void change();
+    abstract void write();
+    public void perform(){
+        read();
+        change();
+        write();
+    }
+}
