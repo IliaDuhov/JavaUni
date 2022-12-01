499d0028f566210838432f80816338a6942e0d3c
IliaDuhov <108584791+IliaDuhov@users.noreply.github.com>
2022-12-01 22:13:20 +0400

    Update Red.java

--- DifferentTasks/TrafficLight(No sinleton)/Red.java
+++ DifferentTasks/TrafficLight(No sinleton)/Red.java
@@ -1,10 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Singleton;
-
-
 public class Red implements Colourable{
     public String toString(){
         return "Red";
