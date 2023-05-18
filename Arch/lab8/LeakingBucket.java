9ed89be6b0a637571faddacb2a7407f88bb28690
Ilia <dukhov03@mail.ru>
2023-05-18 15:31:31 +0400

    Update LeakingBucket.java

--- Arch/lab8/LeakingBucket.java
+++ Arch/lab8/LeakingBucket.java
@@ -34,3 +34,20 @@ public class LeakingBucket {
         return leakRate;
     }
 }
+
+
+public class TrafficController {
+    private LeakingBucket leakingBucket;
+
+    public TrafficController() {
+        leakingBucket = new LeakingBucket(100, 10); // максимальный объем - 100, скорость вытекания - 10
+    }
+
+    public void handleTraffic(int volume) {
+        leakingBucket.addTraffic(volume);
+        while (leakingBucket.getCurrentVolume() > 0) {
+            // обрабатываем трафик
+            leakingBucket.leak();
+        }
+    }
+}
